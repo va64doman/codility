6811bfd5d27c4b35c144702dfc2be4de95ab3e5d
Vandolism <va64do@gmail.com>
2022-12-19 17:18:16 +0800

    update minmaxdiv

--- L14-Binary-search-algorithm/minMaxDiv.py
+++ L14-Binary-search-algorithm/minMaxDiv.py
@@ -1,32 +1,35 @@
 #MinMaxDivision: Divide array A into K blocks and minimize the largest sum of any block.
-def checkBlocks(A, g):
-    blocks = 1
-    blockSum = 0
-    for a in A:
-        blockSum += a
-        if blockSum > g:
-            blockSum = a
-            blocks += 1
-    return blocks
+def resulting_blocks(target_no_of_blocks, numbers, target_sum):
+    """Count blocks that have a sum at most equal to target_sum."""
+    result, temp_sum = 0, 0
+    for number in numbers:
+        if temp_sum + number > target_sum:
+            result += 1
+            temp_sum = number
+        else:
+            temp_sum += number
+    result += 1
+    return max(result, target_no_of_blocks)
+    pass
 
 def minMaxDiv(K, M, A):
-    minV = 0
-    maxV = 0
-    for a in A:
-        maxV += a
-        minV = min(minV, a)
+    upper, lower, result = sum(A), max(A), -1
+    # main binary search loop
+    while upper >= lower:
+        # get mid point
+        mid = (upper + lower) // 2
+        # split array into blocks summing up the mid point       
+        blocks = resulting_blocks(K, A, mid)
         
-    bestAns = maxV
-    while minV <= maxV:
-        mid = (minV + maxV) // 2
-        blocks = checkBlocks(A, mid)
+        # the sweet spot's above mid point       
         if blocks > K:
-            minV = mid + 1
-        else:
-            maxV = mid - 1
-            if mid < bestAns:
-                bestAns = mid
-    return bestAns
+            lower = mid + 1
+        # found a minimal large sum candidate
+        # seek an even lower candidate in [lower, mid-1]
+        elif blocks == K:
+            result = mid if result == -1 else min(result, mid)
+            upper = mid - 1
+    return result
     pass
 
 #(K = 3, M = 5, A = [2,1,5,1,2,2,2]) = 6
