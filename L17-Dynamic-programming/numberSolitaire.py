eed7fa3faba02e93e0397e0f8cd1e4534f481287
Vandolism <va64do@gmail.com>
2022-12-19 17:33:29 +0800

    update number solitaire

--- L17-Dynamic-programming/numberSolitaire.py
+++ L17-Dynamic-programming/numberSolitaire.py
@@ -1,16 +1,15 @@
 #NumberSolitaire: In a given array, find the subset of maximal sum in which the distance between consecutive elements is at most 6.
+import sys
 def numSol(A):
-    R = [0] * len(A)
-    for i in range(len(R)):
-        if i == 0:
-            R[i] = A[i]
-        else:
-            maxOnSquare = -10001
-            for j in range(1, 7):
-                if i - j >= 0:
-                    maxOnSquare = max(maxOnSquare, R[i-j] + A[i])
-            R[i] = maxOnSquare
-    return R[len(R) - 1]        
+    len_a = len(A)
+    dp = [A[0]] + ([0]*(len_a-1))
+    for index in range(1, len_a):
+        temp_max = (-sys.maxsize)-1
+        for die_number in range(1, 6+1):
+            if index-die_number >= 0:
+                temp_max = max( dp[index-die_number] + A[index], temp_max ) # super important!
+        dp[index] = temp_max
+    return dp[len_a-1]     
     pass
 
 #[1,-2,0,9,-1,-2] = 8
